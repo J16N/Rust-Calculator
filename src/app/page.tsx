feab97a541e83f4bd7c3f721a1e62cb75135102a
J16N <jishan2001bhattacharya@gmail.com>
2024-12-18 11:02:30 +0530

    Show errors and add animations.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,36 +8,68 @@ import init, { execute } from "rust-calc";
 export default function Home() {
   let wasm_init = false;
   const [value, setValue] = useState("");
+  const [items, setItems] = useState<string[]>([]);
+  const [modal, showModal] = useState(false);
+  const [modalContent, setModalContent] = useState("");
+  let modalTimeout: NodeJS.Timeout;
 
   return (
-    <div className={styles.page}>
-      <div className={styles.history}></div>
-      <input
-        placeholder="Enter expression"
-        type="text"
-        value={value}
-        onChange={e => {
-          if (e.target.value.match(/[^0-9\+\-\*\/\(\)\.]+/g)) {
+    <>
+      <div className={styles.history}>
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+      <div className={styles.input}>
+        <input
+          placeholder="Enter expression"
+          type="text"
+          value={value}
+          onChange={e => {
+            if (e.target.value.match(/[^0-9\+\-\*\/\(\)\.]+/g)) {
+              return;
+            }
+            setValue(e.target.value);
+          }} />
+        <button className={styles.submitButton} onClick={async () => {
+          if (!wasm_init) {
+            wasm_init = true;
+            await init();
+          }
+          if (value === "") {
             return;
           }
-          setValue(e.target.value);
-        }} />
-      <button className={styles.submitButton} onClick={async () => {
-        if (!wasm_init) {
-          wasm_init = true;
-          await init();
-        }
-        if (value === "") {
-          return;
-        }
-        try {
-          const result = execute(value);
-          setValue(result);
-        }
-        catch (e) {
-          console.error(e);
-        }
-      }}></button>
-    </div>
+          try {
+            const result = execute(value);
+            const v = [];
+            let no_operators = true;
+            for (const c of value) {
+              if (c === "+" || c === "-" || c === "*" || c === "/") {
+                v.push(` ${c} `);
+                no_operators = false;
+              }
+              else {
+                v.push(c);
+              }
+            }
+            const expr = v.join("");
+            const res = no_operators ? `${expr}` : `${expr} = ${result}`;
+            setItems([...items, res]);
+          }
+          catch (err) {
+            clearTimeout(modalTimeout);
+            setModalContent((err as Error).toString());
+            showModal(true);
+
+            modalTimeout = setTimeout(() => {
+              showModal(false);
+            }, 3000);
+          }
+        }}></button>
+      </div>
+      <div className={styles.modal + " " + (modal ? styles.showModal : "")}>{modalContent}</div >
+    </>
   );
 }
